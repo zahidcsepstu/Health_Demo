49e1216ca04a45eb15ddc81411a5cc3b696fdb97
zahid <zahid13@cse.pstu.ac.bd>
2020-07-23 17:43:30 +0600

    CustomSeekBar with custom_thumb.xml implemented

--- app/src/main/java/com/example/healthdemo/BloodPressure.java
+++ app/src/main/java/com/example/healthdemo/BloodPressure.java
@@ -2,15 +2,20 @@ package com.example.healthdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 import com.cunoraz.gifview.library.GifView;
 import com.github.mikephil.charting.animation.ChartAnimator;
@@ -34,7 +39,15 @@ public class BloodPressure extends AppCompatActivity {
 
     Button button;
     HorizontalBarChart barChart;
+    CustomSeekBar seekBar;
+    private float totalSpan = 1000;
+    private float redSpan = 300;
+    private float blueSpan = 300;
+    private float greenSpan = 400;
+    private ArrayList<ProgressItem> progressItemList;
+    private ProgressItem mProgressItem;
     int[] color=new int[]{Color.RED,Color.YELLOW,Color.GREEN};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +57,20 @@ public class BloodPressure extends AppCompatActivity {
 
 
         button=findViewById(R.id.button);
-        barChart=findViewById(R.id.bar_chart);
-        BarDataSet barDataSet= new BarDataSet(dataValues(),"bar data set");
-        barDataSet.setColors(color);
-        BarData barData=new BarData(barDataSet);
-        barChart.setData(barData);
-        barChart.getAxisLeft().setEnabled(false);
-        barChart.getAxisRight().setDrawGridLines(false);
-        barChart.getXAxis().setDrawGridLines(false);
-        barChart.getDescription().setEnabled(false);
-        barChart.getAxisRight().setEnabled(false);
-        barChart.getXAxis().setEnabled(false);
+        seekBar=(CustomSeekBar)findViewById(R.id.custom_seek_bar);
+        initDataToSeekbar();
+
+       // barChart=findViewById(R.id.bar_chart);
+//        BarDataSet barDataSet= new BarDataSet(dataValues(),"bar data set");
+//        barDataSet.setColors(color);
+//        BarData barData=new BarData(barDataSet);
+//        barChart.setData(barData);
+//        barChart.getAxisLeft().setEnabled(false);
+//        barChart.getAxisRight().setDrawGridLines(false);
+//        barChart.getXAxis().setDrawGridLines(false);
+//        barChart.getDescription().setEnabled(false);
+//        barChart.getAxisRight().setEnabled(false);
+//        barChart.getXAxis().setEnabled(false);
         final GifView gifView=findViewById(R.id.gif2);
 
         gifView.play();
@@ -72,11 +88,42 @@ public class BloodPressure extends AppCompatActivity {
         });
     }
 
-    private ArrayList<BarEntry> dataValues(){
-        ArrayList<BarEntry> data = new ArrayList<>();
-        data.add(new BarEntry(0, new float[]{2,5.5f,4}));
-        data.add(new BarEntry(2, new float[]{2,6.5f,4}));
-        return  data;
+    private void initDataToSeekbar() {
+        progressItemList = new ArrayList<ProgressItem>();
+        // red span
+        mProgressItem = new ProgressItem();
+        mProgressItem.progressItemPercentage = ((redSpan / totalSpan) * 100);
+        Log.i("Mainactivity", mProgressItem.progressItemPercentage + "");
+        mProgressItem.color = R.color.yellow;
+        progressItemList.add(mProgressItem);
+        // blue span
+        mProgressItem = new ProgressItem();
+        mProgressItem.progressItemPercentage = (blueSpan / totalSpan) * 100;
+        mProgressItem.color = R.color.green;
+        progressItemList.add(mProgressItem);
+        // green span
+        mProgressItem = new ProgressItem();
+        mProgressItem.progressItemPercentage = (greenSpan / totalSpan) * 100;
+        mProgressItem.color = R.color.red;
+        progressItemList.add(mProgressItem);
+
+
+        seekBar.initData(progressItemList);
+        seekBar.invalidate();
     }
+
+//    private ArrayList<BarEntry> dataValues(){
+//        ArrayList<BarEntry> data = new ArrayList<>();
+//        data.add(new BarEntry(0, new float[]{2,5.5f,4}));
+//        data.add(new BarEntry(2, new float[]{2,6.5f,4}));
+//        return  data;
+//    }
 }
+
+class ProgressItem {
+
+    public int color;
+    public float progressItemPercentage;
+}
+
 
